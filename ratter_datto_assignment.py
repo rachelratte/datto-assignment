c6c08b4bf7c5bfb74804599ce2088d012f3b6fd2
rachelratte <rratte97@gmail.com>
2018-02-26 17:17:28 -0500

    Add files via upload

--- ratter_datto_assignment.py
+++ ratter_datto_assignment.py
@@ -11,14 +11,14 @@ Requires: Pandas and pyarrow (or fastparquet)
 """
 import pandas as pd
 
-#read the target collums from the csv in to a dataframe. Assumes csv is in the same directory as this *.py file. 
+#read the target colums from the csv in to a dataframe. Assumes csv is in the same directory as this *.py file. 
 #To debug, use nrows=# where # is a manageable number that doesn't take too long to load
-df = pd.read_csv('quandl_full_data.csv', usecols=['ticker', 'date', 'open', 'close'], nrows = 20)
+df = pd.read_csv('quandl_full_data.csv', usecols=['ticker', 'date', 'open', 'close'])
 
 #drop rows that presumably have incorrect data (stocks' open or close should never be 0 or negative)
 df[(df[['open','close']] > 0).all(1)]
 
-#adds a new collumn 'average' to the dataframe where each row average[n] is the mean of open[n] and close[n]
+#adds a new column 'average' to the dataframe where each row average[n] is the mean of open[n] and close[n]
 df['average'] = df[['open', 'close']].mean(axis=1)
 
 #exports the dataframe to a parquet file in the same directory as this *.py file
